5ed97d87a6b52a0f4aaada1ff417ae1b424d3355
phshbone <phshbone@gmail.com>
2026-08-26 20:36:06 -0400

    Version 5: bump service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='samsung-remote-v4';
+const CACHE='samsung-remote-v5';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
