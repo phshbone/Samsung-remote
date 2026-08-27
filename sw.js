11c246b174102cb2e34ef1891b079e366dc6bdd4
phshbone <phshbone@gmail.com>
2026-08-26 22:34:24 -0400

    Version 7: refresh service worker cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='samsung-remote-v6';
+const CACHE='samsung-remote-v7';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()])));
